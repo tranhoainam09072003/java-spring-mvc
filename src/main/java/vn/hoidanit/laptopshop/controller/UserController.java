e5613a6da7b4a7ae0abb1ebf38d388f7db2d8832
Hoài Nam <tranhoainam09072003@gmail.com>
2026-05-19 08:09:11 +0700

    import model and add tag lib

--- src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
+++ src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
@@ -1,6 +1,7 @@
 package vn.hoidanit.laptopshop.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -10,16 +11,7 @@ import vn.hoidanit.laptopshop.service.UserService;
 
 import org.springframework.web.bind.annotation.GetMapping;
 
-// @Controller
-// public class UserController {
-
-//     @RequestMapping("/")
-//     public String getHomePage() {
-//         return "hello from controller";
-//     }
-// }
-
-@RestController
+@Controller
 public class UserController {
 
     private UserService userService;
@@ -28,8 +20,26 @@ public class UserController {
         this.userService = userService;
     }
 
-    @GetMapping("")
-    public String getHomePage() {
-        return this.userService.handleHello();
+    @RequestMapping("/")
+    public String getHomePage(Model model) {
+        String test = this.userService.handleHello();
+        model.addAttribute("eric", test);
+        model.addAttribute("hoainam", "from controller with model");
+        return "hello";
     }
 }
+
+// @RestController
+// public class UserController {
+
+// private UserService userService;
+
+// public UserController(UserService userService) {
+// this.userService = userService;
+// }
+
+// @GetMapping("")
+// public String getHomePage() {
+// return this.userService.handleHello();
+// }
+// }
---
+++ src/main/webapp/WEB-INF/view/hello.jsp
@@ -0,0 +1,23 @@
+<%@page contentType="text/html" pageEncoding="UTF-8" %>
+    <%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+
+        <html lang="en">
+
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>Document</title>
+        </head>
+
+        <body>
+            hello from jsp
+            <h1>
+                ${eric}
+            </h1>
+
+            <h2>
+                ${hoainam}
+            </h2>
+        </body>
+
+        </html>
